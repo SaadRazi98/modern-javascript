158ffdbf1fb47d20e133244ee6ddf3c9d4a39978
SAAD <SAADRAZI1998@GMAIL.com>
2023-05-10 13:37:13 -0500

    second push

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var datetime = moment().startOf("day").fromNow();
-console.log(datetime);
+import moment from 'moment';
+console.log(moment().format());
 
-console.log("hello") 
+console.log('hello i am in js')
+
+console.log(moment().format('dddd'));
+console.log(moment().format("MMM Do YY"));
+
+var datetime = moment().startOf("day")
